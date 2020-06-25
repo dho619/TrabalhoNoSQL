ade4718f59f64a9b0f6de94f4b7a33319a727885
Geovane Barbosa <geovanebarbosacc@hotmail.com>
2020-06-25 00:02:38 -0300

    Banco Neo4j funcionando

--- dbNeo4j.py
+++ dbNeo4j.py
@@ -1,4 +1,12 @@
 from neo4jrestclient.client import GraphDatabase
 
 # conecta ao banco
-db = GraphDatabase("http://localhost:7474", username="neo4j", password="1.618_3,14")
+dbNeo4j = GraphDatabase("http://localhost:7474", username="neo4j", password="1.618_3,14")
+
+dbNeo4j.query('MATCH (n) DETACH DELETE n', returns=())#limpar banco
+
+people = dbNeo4j.labels.create("Person")
+alice = dbNeo4j.nodes.create(name="Alice", age=30)
+bob = dbNeo4j.nodes.create(name="Bob", age=30)
+people.add(alice, bob)
+alice.relationships.create("Conhece", bob, since=1980)
